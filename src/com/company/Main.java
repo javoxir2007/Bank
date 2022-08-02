c44b1575269e5ef1b04e0d89d7e3255e2e4e838e
DilbekErgashev1 <ergashevdilbek9@gmail.com>
2022-08-01 23:43:46 -0400

    teskari chiqarish

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scanner = new Scanner(System.in);
+
+
+        for (int i = 10; i >= 1 ; i--) {
+            System.out.print(i+","); //1 2 3 4
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
