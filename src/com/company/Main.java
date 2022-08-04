87d32f6bb85576e530b9969d4aa2beadeb6b97bd
DilbekErgashev1 <ergashevdilbek9@gmail.com>
2022-08-04 00:45:15 -0400

    Bo`linadigan sonlar

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,56 +1,30 @@
 package com.company;
 
+import java.sql.PreparedStatement;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("BankUZB");
-        for (; ; ) {
-            System.out.println("Next -> 1");
-            System.out.println("Quit ->");
-            System.out.println(" = ");
-            int n = scanner.nextInt();
 
-            if (n == 1) {
-                System.out.println("Name wtite");
-               scanner = new Scanner(System.in);
-                String name = scanner.nextLine();
-                System.out.println("Cart number write");
+        int a = scanner.nextInt();
+        int i = 1;
+        int res = 0;
 
-                int cart = scanner.nextInt();
-                System.out.println("Write money");
-                int money = scanner.nextInt();
-
-                System.out.println("Year write :");
-                int year = scanner.nextInt();
-                double p = 23;
-               double res = 0;
-                double f = money * p / 100;
-
-                for (int i = 1; i <= year; i++) {
-
-                  f = i;
-                 res = f + money;
-                }
-                System.out.println("Mr " + name + " " + res + " $");
-
-            } else if (n == 0) {
-                break;
-            } else {
-                System.out.println("Write inorrect number !!!");
+        while (i <= 100) {
+            if (i % a == 0) {
+                res++;
+                System.out.print(i+ " ");
             }
-
-
+            i++;
         }
+        System.out.println();
+        System.out.println(res);
+
     }
 }
-
-
-
-
-
 
 
